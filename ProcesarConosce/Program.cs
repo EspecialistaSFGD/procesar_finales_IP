ea67c423d3868eefb3d95eca3411c8f59af33d23
Julio Leonardo Paredes <julio.leonardo.developer@gmail.com>
2022-04-05 16:03:39 -0500

    Copia de reportes conosce hacia tablas de base de datos

--- ProcesarConosce/Program.cs
+++ ProcesarConosce/Program.cs
@@ -10,6 +10,7 @@ namespace ProcesarConosce
         private static IConfigurationRoot Configuration { get; set; }
         static void Main(string[] args)
         {
+            var anioProceso = "";
             var conexionBd = "";
             var servidorSmtp = "";
             var puertoSmtp = "";
@@ -26,19 +27,20 @@ namespace ProcesarConosce
 
             try
             {
-                conexionBd = string.IsNullOrEmpty(args[0]) ? "" : args[0];
-                servidorSmtp = string.IsNullOrEmpty(args[1]) ? "" : args[1];
-                puertoSmtp = string.IsNullOrEmpty(args[2]) ? "" : args[2];
-                usuarioSmtp = string.IsNullOrEmpty(args[3]) ? "" : args[3];
-                claveSmtp = string.IsNullOrEmpty(args[4]) ? "" : args[4];
-                deSmtp = string.IsNullOrEmpty(args[5]) ? "" : args[5];
-                paraSmtp = string.IsNullOrEmpty(args[6]) ? "" : args[6];
-                hostSftp = string.IsNullOrEmpty(args[7]) ? "" : args[7];
-                puertoSftp = string.IsNullOrEmpty(args[8]) ? "" : args[8];
-                usuarioSftp = string.IsNullOrEmpty(args[9]) ? "" : args[9];
-                claveSftp = string.IsNullOrEmpty(args[10]) ? "" : args[10];
-                rutaOrigen = string.IsNullOrEmpty(args[11]) ? "" : args[11];
-                rutaDestino = string.IsNullOrEmpty(args[12]) ? "" : args[12];
+                anioProceso = string.IsNullOrEmpty(args[0]) ? "" : args[0];
+                conexionBd = string.IsNullOrEmpty(args[1]) ? "" : args[1];
+                servidorSmtp = string.IsNullOrEmpty(args[2]) ? "" : args[2];
+                puertoSmtp = string.IsNullOrEmpty(args[3]) ? "" : args[3];
+                usuarioSmtp = string.IsNullOrEmpty(args[4]) ? "" : args[4];
+                claveSmtp = string.IsNullOrEmpty(args[5]) ? "" : args[5];
+                deSmtp = string.IsNullOrEmpty(args[6]) ? "" : args[6];
+                paraSmtp = string.IsNullOrEmpty(args[7]) ? "" : args[7];
+                hostSftp = string.IsNullOrEmpty(args[8]) ? "" : args[8];
+                puertoSftp = string.IsNullOrEmpty(args[9]) ? "" : args[9];
+                usuarioSftp = string.IsNullOrEmpty(args[10]) ? "" : args[10];
+                claveSftp = string.IsNullOrEmpty(args[11]) ? "" : args[11];
+                rutaOrigen = string.IsNullOrEmpty(args[12]) ? "" : args[12];
+                rutaDestino = string.IsNullOrEmpty(args[13]) ? "" : args[13];
             }
             catch (Exception)
             {
@@ -70,15 +72,21 @@ namespace ProcesarConosce
                 SourceRoute = !string.IsNullOrEmpty(rutaOrigen) ? rutaOrigen : Configuration.GetSection("RutaOrigen").Value.ToString(),
                 DestinationRoute = !string.IsNullOrEmpty(rutaDestino) ? rutaDestino : Configuration.GetSection("RutaDestino").Value.ToString()
             };
-            EjecutarProceso(conexionBd, mailConfiguration, sftprequest);
-                //.GetAwaiter()
-                //.GetResult();
+            EjecutarProceso(
+                !string.IsNullOrEmpty(conexionBd) ? conexionBd : Configuration.GetConnectionString("ConexionPcm"),
+                mailConfiguration, 
+                sftprequest,
+                !string.IsNullOrEmpty(anioProceso) ? anioProceso : Configuration.GetSection("AnioCarga").Value.ToString()
+                )
+                .GetAwaiter()
+                .GetResult();
         }
 
 
-        static /*async Task*/ void EjecutarProceso(string conexion, Mail mail, FileManager.SftpRequest sftp)
+        static async Task EjecutarProceso(string conexion, Mail mail, FileManager.SftpRequest sftp, string anioProceso)
         {
-            var mensajeRespuesta = @"<h3>Proceso de extraccion de reportes desde CONOSCE</h3><p>mensaje_respuesta</p>";
+            var mensajeRespuesta = @"<h3>Proceso de extraccion de reportes desde CONOSCE</h3><h4>Procesos conformes</h4>mensaje_ok<h4>Procesos Errados</h4>mensaje_error";
+
             var repositorio = new Repositorio(conexion);
             try
             {
@@ -92,34 +100,58 @@ namespace ProcesarConosce
                 Console.WriteLine($"Descargando los reportes de contratos de CONOSCE");
                 sftp.SourceRoute = $"{rutaBaseOrigen}conosce_contratos_s3/";
                 sftp.DestinationRoute = $"{rutaBaseDestino}conosce_contratos_s3\\";
-                var haDescargadoContratos = repositorio.RecuperarReportesConosce(sftp);
 
-                if (haDescargadoContratos)
+                var haDescargadoContratos = true;//repositorio.RecuperarReportesConosce(sftp);
+
+                if (!haDescargadoContratos)
                 {
-                    Console.WriteLine($"Los reportes de contratos han sido descargados correctamente de CONOSCE");
+                    Console.WriteLine("Los reportes de contratos no han podido ser descargados correctamente de CONOSCE.");
+                }
+                else
+                {
+                    Console.WriteLine("Los reportes de contratos han sido descargados correctamente de CONOSCE.");
+                    Console.WriteLine("Cargando reportes descargados a la tabla de contratos.");
+                    var haProcesadoDatosContratos = await repositorio.CargarDataTablaContrato($"{sftp.DestinationRoute}{anioProceso}\\", anioProceso);
+                    Console.WriteLine(haProcesadoDatosContratos ? "Los contratos se cargaron correctamente en la tabla de contratos." : "No se cargó la información correctamente en la tabla de contratos.");
                 }
 
                 Console.WriteLine($"Descargando los reportes de cronogramas de CONOSCE");
                 sftp.SourceRoute = $"{rutaBaseOrigen}conosce_cronograma/";
                 sftp.DestinationRoute = $"{rutaBaseDestino}conosce_cronograma\\";
 
-                var haDescargadoCronogramas = repositorio.RecuperarReportesConosce(sftp);
+                var haDescargadoCronogramas = true;//repositorio.RecuperarReportesConosce(sftp);
 
-                if (haDescargadoCronogramas)
+                if (!haDescargadoCronogramas)
                 {
+                    Console.WriteLine("Los reportes de cronogramas no han podido ser descargados correctamente de CONOSCE.");
+                }
+                else
+                {
+
                     Console.WriteLine($"Los reportes de cronogramas han sido descargados correctamente de CONOSCE");
+                    Console.WriteLine("Cargando reportes descargados a la tabla de cronogramas.");
+                    var haProcesadoDatosCronogramas = await repositorio.CargarDataTablaCronograma($"{sftp.DestinationRoute}{anioProceso}\\", anioProceso);
+                    Console.WriteLine(haProcesadoDatosCronogramas ? "Los cronogramas se cargaron correctamente en la tabla de cronogramas." : "No se cargó la información correctamente en la tabla de cronogramas.");
                 }
 
                 Console.WriteLine($"Descargando los reportes especiales de CONOSCE");
                 sftp.SourceRoute = $"{rutaBaseOrigen}reporte_especial/";
                 sftp.DestinationRoute = $"{rutaBaseDestino}reporte_especial\\";
 
-                var haDescargadoReporteEspecial = repositorio.RecuperarReportesConosce(sftp);
+                var haDescargadoReporteEspecial = true;//repositorio.RecuperarReportesConosce(sftp);
 
-                if (haDescargadoReporteEspecial)
+                if (!haDescargadoReporteEspecial)
+                {
+                    Console.WriteLine("Los reportes especiales no han podido ser descargados correctamente de CONOSCE.");
+                }
+                else
                 {
                     Console.WriteLine($"Los reportes especiales han sido descargados correctamente de CONOSCE");
+                    Console.WriteLine("Cargando reportes descargados a la tabla de snip.");
+                    var haProcesadoDatosSnip = await repositorio.CargarDataTablaSnip(sftp.DestinationRoute, anioProceso);
+                    Console.WriteLine(haProcesadoDatosSnip ? "Los snip se cargaron correctamente en la tabla de snip." : "No se cargó la información correctamente en la tabla de snip.");
                 }
+
 
                 var detalle = $"El proceso de extracción de reportes desde CONOSCE se ha completado correctamente, puede revisar los archivos en el servidor o consultar directamente desde la base de datos y/o Web del Mapa Inversiones</p>";
                 mensajeRespuesta = mensajeRespuesta.Replace("mensaje_respuesta", detalle);
@@ -129,7 +161,7 @@ namespace ProcesarConosce
             }
             catch (Exception exception)
             {
-                var detalle = $"<p>Ocurrió un problema durante el proceso de extracción de reportes. Detalle del error : {exception.Message}</p>";
+                var detalle = $"Ocurrió un problema durante el proceso de extracción de reportes. Detalle del error : {exception.Message}";
                 mensajeRespuesta = mensajeRespuesta.Replace("mensaje_respuesta", detalle);
                 repositorio.SendMail(mail, "Proceso de Carga Masiva de Datos de Proyectos", mensajeRespuesta);
 
--- ProcesarConosce/Repositorio.cs
+++ ProcesarConosce/Repositorio.cs
@@ -9,22 +9,27 @@ using System.Collections.Generic;
 using Renci.SshNet.Sftp;
 using System.IO;
 using System.Linq;
+using System.Data;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Dapper;
 
 namespace ProcesarConosce
 {
     public class Repositorio
     {
         private readonly string Conexion = "";
-        private readonly FileManager fileManager;
-        private readonly TypeConvertionManager typeConvertionsManager;
+        private readonly int TiempoEsperaCargadoMasivo;
+        private readonly int BatchSize;
 
         public Repositorio(string conexion)
         {
             Conexion = conexion;
-            fileManager = FileManager.GetNewFileManager();
-            typeConvertionsManager = TypeConvertionManager.GetNewTypeConvertionManager();
+            TiempoEsperaCargadoMasivo = 10000;
+            BatchSize = 50000;
         }
 
+        //Paso 1.- Recupera reportes desde el SFTP de CONOSCE
         public bool RecuperarReportesConosce(FileManager.SftpRequest sftpRequest)
         {
             try
@@ -54,7 +59,297 @@ namespace ProcesarConosce
             }
         }
 
+        //Pase 2.- Prepara datos de contratos desde los reportes descargados de CONOSCE
+        public async Task<bool> CargarDataTablaContrato(string urlBase, string anioProceso)
+        {
+            try
+            {
 
+                var directorio = new DirectoryInfo(urlBase);
+                var dataTableContrato = new DataTable();
+                var indice = 0;
+                foreach (var archivo in directorio.GetFiles($"CONOSCE_CONTRATOS{anioProceso}_?.xlsx"))
+                {
+                    if (indice == 0)
+                    {
+                        dataTableContrato = ObtenerDataTableDesdeExcel(archivo.FullName, Tuple.Create(2, 1), true);
+                        var columna = dataTableContrato.Columns.Add("ANIO", Type.GetType("System.Int32"), anioProceso);
+                        columna.SetOrdinal(0);
+                    }
+                    else
+                    {
+                        var dataTableContratoDetalle = new DataTable();
+                        dataTableContratoDetalle = ObtenerDataTableDesdeExcel(archivo.FullName, Tuple.Create(2, 1), true);
+                        var column = dataTableContratoDetalle.Columns.Add("ANIO", Type.GetType("System.Int32"), anioProceso);
+                        column.SetOrdinal(0);
+
+                        dataTableContrato.Merge(dataTableContratoDetalle);
+
+                    }
+                    indice++;
+                }
+
+                var contratosHanSidoEliminados = await EliminarContratos();
+
+                if (!contratosHanSidoEliminados)
+                {
+                    throw new Exception("Los contratos existentes en la tabla no han podido ser eliminados.");
+                }
+
+                var contratosHanSidoInsertados = InsercionMasivaContratos(dataTableContrato);
+
+                if (!contratosHanSidoInsertados)
+                {
+                    throw new Exception("Los contratos extraidos del reporte de CONOSCE no han podido ser registrados.");
+                }
+
+                return true;
+            }
+            catch (Exception exception)
+            {
+                throw new Exception($"No se ha completado el proceso de carga masiva de contratos: Detalle del error => {exception.Message}" ,exception);
+            }
+
+        }
+
+        //Paso 3.- Eliminar contratos para el anio en proceso
+        public async Task<bool> EliminarContratos()
+        {
+            using var conexionSql = new SqlConnection(Conexion);
+            try
+            {
+                conexionSql.Open();
+
+                var respuesta = await conexionSql.QueryAsync("dbo.05A_EliminarContratoConosce", commandType: CommandType.StoredProcedure, commandTimeout: 1200);
+                return true;
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+                throw;
+            }
+            finally
+            {
+                conexionSql.Close();
+            }
+
+        }
+
+        //Paso 4.- Registrar Contratos para el anio de proceso de forma masiva
+        public bool InsercionMasivaContratos(DataTable valores)
+        {
+            using var conexionSql = new SqlConnection(Conexion);
+            conexionSql.Open();
+
+            using SqlBulkCopy bulkCopy = new(conexionSql);
+            bulkCopy.BulkCopyTimeout = TiempoEsperaCargadoMasivo;
+            bulkCopy.BatchSize = BatchSize;
+            bulkCopy.DestinationTableName = "dbo.CONOSCE_CONTRATOS";
+
+            try
+            {
+                bulkCopy.WriteToServer(valores);
+                return true;
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+                throw;
+
+            }
+            finally
+            {
+                conexionSql.Close();
+            }
+        }
+
+        //Pase 5.- Prepara datos de cronogramas desde los reportes descargados de CONOSCE
+        public async Task<bool> CargarDataTablaCronograma(string urlBase, string anioProceso)
+        {
+            try
+            {
+
+                var directorio = new DirectoryInfo(urlBase);
+                var dataTableCronogramas = new DataTable();
+                var indice = 0;
+                foreach (var archivo in directorio.GetFiles($"CONOSCE_CRONOGRAMA{anioProceso}_?.xlsx"))
+                {
+                    if (indice == 0)
+                    {
+                        dataTableCronogramas = ObtenerDataTableDesdeExcel(archivo.FullName, Tuple.Create(2, 1), true);
+                        var columna = dataTableCronogramas.Columns.Add("ANIO", Type.GetType("System.Int32"), anioProceso);
+                        columna.SetOrdinal(0);
+                    }
+                    else
+                    {
+                        var dataTableCronogramasDetalle = new DataTable();
+                        dataTableCronogramasDetalle = ObtenerDataTableDesdeExcel(archivo.FullName, Tuple.Create(2, 1), true);
+                        var column = dataTableCronogramasDetalle.Columns.Add("ANIO", Type.GetType("System.Int32"), anioProceso);
+                        column.SetOrdinal(0);
+
+                        dataTableCronogramas.Merge(dataTableCronogramasDetalle);
+
+                    }
+                    indice++;
+                }
+
+                var cronogramasHanSidoEliminados = await EliminarCronogramas();
+
+                if (!cronogramasHanSidoEliminados)
+                {
+                    throw new Exception("Los cronogramas existentes en la tabla no han podido ser eliminados.");
+                }
+
+                var cronogramasHanSidoRegistrados = InsercionMasivaCronogramas(dataTableCronogramas);
+
+                if (!cronogramasHanSidoRegistrados)
+                {
+                    throw new Exception("Los cronogramas extraidos del reporte de CONOSCE no han podido ser registrados.");
+                }
+
+                return true;
+            }
+            catch (Exception exception)
+            {
+                throw new Exception($"No se ha completado el proceso de carga masiva de cronogramas: Detalle del error => {exception.Message}", exception);
+            }
+
+        }
+
+        //Paso 6.- Eliminar cronogramas de convocatoria para el anio en proceso
+        public async Task<bool> EliminarCronogramas()
+        {
+            using var conexionSql = new SqlConnection(Conexion);
+            try
+            {
+                conexionSql.Open();
+
+                var respuesta = await conexionSql.QueryAsync("dbo.05B_EliminarCronogramaConosce", commandType: CommandType.StoredProcedure, commandTimeout: 1200);
+                return true;
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+                throw;
+            }
+            finally
+            {
+                conexionSql.Close();
+            }
+
+        }
+       
+        //Paso 7.- Registrar Cronogramas para el anio de proceso de forma masiva
+        public bool InsercionMasivaCronogramas(DataTable valores)
+        {
+            using var conexionSql = new SqlConnection(Conexion);
+            conexionSql.Open();
+
+            using SqlBulkCopy bulkCopy = new(conexionSql);
+            bulkCopy.BulkCopyTimeout = TiempoEsperaCargadoMasivo;
+            bulkCopy.BatchSize = BatchSize;
+            bulkCopy.DestinationTableName = "dbo.CONOSCE_CRONOGRAMA";
+
+            try
+            {
+                bulkCopy.WriteToServer(valores);
+                return true;
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+                throw;
+
+            }
+            finally
+            {
+                conexionSql.Close();
+            }
+        }
+
+        //Pase 8.- Prepara datos de cronogramas desde los reportes descargados de CONOSCE
+        public async Task<bool> CargarDataTablaSnip(string urlBase, string anioProceso)
+        {
+            try
+            {
+                var dataTableSnip = new DataTable();
+                dataTableSnip = ObtenerDataTableDesdeExcel($"{urlBase}SNIP SEACE.xlsx", Tuple.Create(1, 1), true);
+
+                var snipHanSidoEliminados = await EliminarSnip();
+
+                if (!snipHanSidoEliminados)
+                {
+                    throw new Exception("Los snip existentes en la tabla no han podido ser eliminados.");
+                }
+
+                var snipHanSidoInsertados = InsercionMasivaSnip(dataTableSnip);
+
+                if (!snipHanSidoInsertados)
+                {
+                    throw new Exception("Los snip extraidos del reporte especial de CONOSCE no han podido ser registrados.");
+                }
+
+                return true;
+            }
+            catch (Exception exception)
+            {
+                throw new Exception($"No se ha completado el proceso de carga masiva de snip: Detalle del error => {exception.Message}", exception);
+            }
+
+        }
+
+        //Paso 9.- Eliminar snip para el anio en proceso
+        public async Task<bool> EliminarSnip()
+        {
+            using var conexionSql = new SqlConnection(Conexion);
+            try
+            {
+                conexionSql.Open();
+
+                var respuesta = await conexionSql.QueryAsync("dbo.05C_EliminarSnipConosce", commandType: CommandType.StoredProcedure, commandTimeout: 1200);
+                return true;
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+                throw;
+            }
+            finally
+            {
+                conexionSql.Close();
+            }
+
+        }
+
+        //Paso 10.- Registrar SNIP de forma masiva
+        public bool InsercionMasivaSnip(DataTable valores)
+        {
+            using var conexionSql = new SqlConnection(Conexion);
+            conexionSql.Open();
+
+            using SqlBulkCopy bulkCopy = new(conexionSql);
+            bulkCopy.BulkCopyTimeout = TiempoEsperaCargadoMasivo;
+            bulkCopy.BatchSize = BatchSize;
+            bulkCopy.DestinationTableName = "dbo.SNIP_SEACE";
+
+            try
+            {
+                bulkCopy.WriteToServer(valores);
+                return true;
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+                throw;
+
+            }
+            finally
+            {
+                conexionSql.Close();
+            }
+        }
+
+        //Paso 11.- Descargar directorio especifico desde el SFTP
         public void DescargarDirectorio(SftpClient clienteSftp, string rutaOrigen, string rutaDestino)
         {
             Directory.CreateDirectory(rutaDestino);
@@ -83,7 +378,36 @@ namespace ProcesarConosce
             }
         }
 
-        //Paso 8.- Enviar mail por concepto de error o éxito
+        //Paso 12.- Convertir excel a DataTable
+        public DataTable ObtenerDataTableDesdeExcel(string rutaArchivo, Tuple<int, int> posicionInicioLibro, bool tieneCabecera = true)
+        {
+            using (var excelPackage = new OfficeOpenXml.ExcelPackage())
+            {
+                using (var stream = File.OpenRead(rutaArchivo))
+                {
+                    excelPackage.Load(stream);
+                }
+                var hojaCalculo = excelPackage.Workbook.Worksheets.First();
+                DataTable tabla = new DataTable();
+                foreach (var primeraFilaCelda in hojaCalculo.Cells[posicionInicioLibro.Item1, posicionInicioLibro.Item2, posicionInicioLibro.Item1, hojaCalculo.Dimension.End.Column])
+                {
+                    tabla.Columns.Add(tieneCabecera ? primeraFilaCelda.Text : string.Format("Column {0}", primeraFilaCelda.Start.Column));
+                }
+                var filaInicial = tieneCabecera ? (posicionInicioLibro.Item1 + 1) : posicionInicioLibro.Item1;
+                for (int numeroFila = filaInicial; numeroFila <= hojaCalculo.Dimension.End.Row; numeroFila++)
+                {
+                    var fila = hojaCalculo.Cells[numeroFila, posicionInicioLibro.Item2, numeroFila, hojaCalculo.Dimension.End.Column];
+                    DataRow row = tabla.Rows.Add();
+                    foreach (var celda in fila)
+                    {
+                        row[celda.Start.Column - 1] = celda.Text;
+                    }
+                }
+                return tabla;
+            }
+        }
+
+        //Paso 13.- Enviar mail por concepto de error o éxito
         public void SendMail(Mail configuracion, string asunto, string mensaje)
         {
             try
